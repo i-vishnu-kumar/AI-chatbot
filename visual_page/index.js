838f311d8c09366eed7fdfb7bb5bd17bc13a5c49
i-vishnu-kumar <80880809+i-vishnu-kumar@users.noreply.github.com>
2025-09-06 23:22:38 +0900

    Update index.js

--- visual_page/index.js
+++ visual_page/index.js
@@ -1,3 +1,37 @@
-document.addEventListener('DOMContentLoaded', async function(){
-    
+document.addEventListener('DOMContentLoaded', async function () {
+    send_btn = document.querySelector(".send")
+    input = document.querySelector("#textbox")
+    screen = document.querySelector(".screen")
+
+    send_btn.addEventListener('click', async function () {
+        let message = input.value;
+
+        if (message) {
+            add_consequently("user", message);
+            input.value = "";
+        }
+        else {
+            add_consequently("ai", "Got empty message!")
+        }
+
+    })
 })
+
+async function add_consequently(className, message) {
+    const new_div = document.createElement('div');
+    new_div.classList.add(className, "msg-box");
+    new_div.textContent = message;
+    screen.appendChild(new_div);
+}
+
+
+
+
+
+
+
+
+
+
+
+
